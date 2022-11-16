3b25f38e6cf0107586224fdaa3d73a409d2f786e
Jordan Sampson <jordansampson23@gmail.com>
2022-11-16 12:16:16 -0600

    Restarted javascript.js

--- javascript.js
+++ javascript.js
@@ -1,38 +1,51 @@
-let userChoice = prompt("Please choose Rock, Paper, or Scissors").toUpperCase();
-console.log(userChoice);
-let computerChoice = Math.random();
-if (computerChoice <= 0.34) {
-    computerChoice = "ROCK";
-} else if (computerChoice <= 0.67){
-    computerChoice = "PAPER";
-} else {
-    computerChoice = "SCISSORS";
-}
-console.log(computerChoice);
-
 let playerScore = 0;
 let computerScore = 0;
-let roundWinner = " ";
+let tie = 0;
 
-function playRound(userChoice, computerChoice){
-    if (userChoice === computerChoice){
-        roundWinner = "Tie"
+function getComputerChoice(){
+    let random = Math.random();
+    if (random <= 0.33){
+        return "ROCK";
+    } else if (random >= 0.66){
+        return "PAPER";
+    } else {
+        return "SCISSORS"
     }
-    if (
+}
+
+function playRound(userChoice, computerChoice) {
+    //Tie breaker
+    if (userChoice === computerChoice){
+        return "Tie!"
+    }
+    else if ( //Player wins scenarios
         (userChoice === "ROCK" && computerChoice === "SCISSORS") ||
         (userChoice === "PAPER" && computerChoice === "ROCK") ||
         (userChoice === "SCISSORS" && computerChoice === "PAPER")
     ){
         playerScore++;
-        roundWinner = "Player"
+        return "You Win!"
     }
-    if (
+     else if ( //Computer wins scenarios
         (userChoice === "ROCK" && computerChoice === "PAPER") ||
         (userChoice === "PAPER" && computerChoice === "SCISSORS") ||
         (userChoice === "SCISSORS" && computerChoice === "ROCK")
     ){
         computerScore++;
-        roundWinner = "Computer"
+        return "Computer Wins!"
     }
 }
-console.log(playRound(userChoice,computerChoice));
+
+function game(){
+    for (var i = 0; i < 5; i++){
+        let playerChoice = prompt("Choose Rock, Paper, or Scissors").toUpperCase();
+        let computerChoice = getComputerChoice();
+        console.log(playRound(playerChoice,computerChoice));
+        console.log(playerScore);
+        console.log(computerScore);
+    }
+}
+
+
+
+console.log(game());
